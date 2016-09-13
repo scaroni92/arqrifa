7012af15e8576bd6f26161738ab24b09d393e4b9
Alekido7 <alekido7@gmail.com>
2016-09-12 22:09:00 -0300

    Implementación de marcarAsistencia en REST
    - Clase Aplicacion: Se registraron las clases Servicio y
    MultiPartFeature.
    - Se reimplementó el método marcarAsistencia en el servicio
    - Se agregaron librerias para dar soporte a MultipartFormData

--- Release/ArquitecturaRifa/src/java/org/arqrifa/rest/Aplicacion.java
+++ Release/ArquitecturaRifa/src/java/org/arqrifa/rest/Aplicacion.java
@@ -1,9 +1,22 @@
 package org.arqrifa.rest;
 
+import java.util.HashSet;
+import java.util.Set;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
+import org.arqrifa.rest.recursos.Servicio;
+import org.glassfish.jersey.media.multipart.MultiPartFeature;
 
 @ApplicationPath("api")
 public class Aplicacion extends Application {
 
+    @Override
+    public Set<Class<?>> getClasses() {
+        final Set<Class<?>> classes = new HashSet<Class<?>>();
+
+        classes.add(Servicio.class);
+        classes.add(MultiPartFeature.class);
+
+        return classes;
+    }
 }
--- Release/ArquitecturaRifa/src/java/org/arqrifa/rest/recursos/Servicio.java
+++ Release/ArquitecturaRifa/src/java/org/arqrifa/rest/recursos/Servicio.java
@@ -5,11 +5,14 @@ import org.arqrifa.datatypes.DTReunion;
 import org.arqrifa.logica.FabricaLogica;
 import org.arqrifa.repository.Repository;
 import java.util.List;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
+import org.glassfish.jersey.media.multipart.FormDataParam;
 
 @Path("/servicio")
 @Produces(MediaType.APPLICATION_JSON)
@@ -20,20 +23,25 @@ public class Servicio {
     public DTUsuario login(@QueryParam("ci") int ci, @QueryParam("pass") String pass) {
         return FabricaLogica.getLogicaUsuario().Autenticar(ci, pass);
     }
-    
-    @Path("/asistencia")
-    @GET
-    public void marcarAsistencia(@QueryParam("ci") int ci){
-        DTUsuario usuario = null;
-        DTReunion reunion = null;
+
+    @POST
+    @Path("/asistencias/marcar")
+    @Consumes({MediaType.MULTIPART_FORM_DATA, MediaType.APPLICATION_JSON})
+    public void marcarAsistencia(@FormDataParam("usuario") DTUsuario usuario, @FormDataParam("reunion") DTReunion reunion) {
+        if (usuario != null) {
+            System.out.println(usuario.getNombre());
+        }
+        if (reunion != null) {
+            System.out.println(reunion.getId());
+        }
         FabricaLogica.getControladorReuniones().MarcarAsistencia(usuario, reunion);
     }
-    
+
     @Path("/reuniones/getActivas")
     @GET
-    public List<DTReunion> getReunionesActias(){
+    public List<DTReunion> getReunionesActivas() {
         //return Repository.reunionesActivas;
         return FabricaLogica.getControladorReuniones().getReunionesActivas();
     }
-    
+
 }
