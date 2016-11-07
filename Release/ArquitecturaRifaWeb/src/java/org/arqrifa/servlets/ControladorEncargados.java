89cf1ea0c0de1d8a13a8c523553e465d615ee439
Alekido7 <alekido7@gmail.com>
2016-11-07 21:44:36 -0200

    ControladorEncargados --> rechazarSolicitud

--- Release/ArquitecturaRifaWeb/src/java/org/arqrifa/servlets/ControladorEncargados.java
+++ Release/ArquitecturaRifaWeb/src/java/org/arqrifa/servlets/ControladorEncargados.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 import org.arqrifa.datatypes.DTSolicitud;
 import org.arqrifa.datatypes.DTUsuario;
-import org.arqrifa.rest.ClienteJersey;
 import org.arqrifa.viewmodels.VMSolicitudes;
 
 public class ControladorEncargados extends Controlador {
@@ -12,7 +11,9 @@ public class ControladorEncargados extends Controlador {
     public void solicitudes_get() {
         List<DTSolicitud> lista = new ArrayList();
         try {
-            lista = new ClienteJersey().listarSolicitudes((DTUsuario) sesion.getAttribute("usuario"));
+
+            lista = new ArrayList(cliente.listarSolicitudes((DTUsuario) sesion.getAttribute("usuario")));
+
             mostrarVista("Vistas/Encargado/solicitudes.jsp", new VMSolicitudes(lista, ""));
             sesion.setAttribute("solicitudes", lista);
         } catch (Exception e) {
@@ -23,24 +24,40 @@ public class ControladorEncargados extends Controlador {
     public void confirmar_get() {
         List<DTSolicitud> solicitudes = new ArrayList();
         try {
-            int ci = Integer.parseInt(request.getParameter("ci"));
-            solicitudes = (List<DTSolicitud>) sesion.getAttribute("solicitudes");
+            solicitudes = (ArrayList)sesion.getAttribute("solicitudes");
             DTSolicitud solicitud = null;
             for (DTSolicitud s : solicitudes) {
-                if (s.getCi() == ci) {
+                if (s.getCi() == Integer.parseInt(request.getParameter("ci"))) {
                     solicitud = s;
                 }
             }
-            
-            if (solicitud == null) {
-                throw new Exception("Error solicitud nula.");
-            }
-            
-            new ClienteJersey().confirmarSolicitud(solicitud);
-            
-            solicitudes = new ClienteJersey().listarSolicitudes((DTUsuario)sesion.getAttribute("usuario"));
+
+            cliente.confirmarSolicitud(solicitud);
+            solicitudes.remove(solicitud);
+
             sesion.setAttribute("solicitudes", solicitudes);
             mostrarVista("Vistas/Encargado/solicitudes.jsp", new VMSolicitudes(solicitudes, "Solicitud confirmada exitosamente."));
+        } catch (Exception e) {
+            mostrarVista("Vistas/Encargado/solicitudes.jsp", new VMSolicitudes(solicitudes, e.getMessage()));
+        }
+    }
+
+    public void rechazar_get() {
+        List<DTSolicitud> solicitudes = new ArrayList();
+        try {
+            solicitudes = (ArrayList)sesion.getAttribute("solicitudes");
+            DTSolicitud solicitud = null;
+            for (DTSolicitud s : solicitudes) {
+                if (s.getCi() == Integer.parseInt(request.getParameter("ci"))) {
+                    solicitud = s;
+                }
+            }
+
+            cliente.rechazarSolicitud(solicitud);
+            solicitudes.remove(solicitud);
+
+            sesion.setAttribute("solicitudes", solicitudes);
+            mostrarVista("Vistas/Encargado/solicitudes.jsp", new VMSolicitudes(solicitudes, "Solicitud rechazada exitosamente."));
         } catch (Exception e) {
             mostrarVista("Vistas/Encargado/solicitudes.jsp", new VMSolicitudes(solicitudes, e.getMessage()));
         }
